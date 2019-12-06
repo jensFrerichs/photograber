fadd0903d8b0a5f324ee985ca40426de32c12f0a
Jens Frerichs <frerichs@yfu.de>
2019-12-06 10:35:53 +0100

    Use id instead of class for flexslider element - make addSlide work

--- app/assets/javascripts/start/index.js
+++ app/assets/javascripts/start/index.js
@@ -2,26 +2,27 @@
 
 
 $(document).ready(function(){
-// --- img slider ---
-
-    $('.flexslider').flexslider();
-
-// --- img slider ---
-
-// --- fetch files logic ---
-    let target = $('#slides');
     let button = $('#trigger');
     let paths = [];
-    function updatePaths () {
-        $('img').each(function(){ paths.push($(this).attr('src')) });
-    }
+// --- img slider ---
+
+    $('#flexslider').flexslider();
+
+// --- img slider ---
+function updatePaths () {
+    $('img').each(function(){ paths.push($(this).attr('src')) });
+}
+
+
+// --- fetch files logic ---
     updatePaths();
     button.on('click', function(){
         $.post( "/fetch", { data: paths }, function(data){
             console.log(data);
             if(data.length >= 1){
                 data.forEach(function(url){
-                    $('.flexslider').data('flexslider').addSlide(url);
+                    let li = "<li>" + url + "</li>";
+                    $('#flexslider').data('flexslider').addSlide($(li));
                 });
                 updatePaths();
             }
